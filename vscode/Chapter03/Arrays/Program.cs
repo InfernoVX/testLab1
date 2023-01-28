be7759f38ab71cf38b4cee1d4c96942116adb4d2
John Doe <johndoe@email.com>
2023-01-28 16:01:14 -0500

    testing

--- vscode/Chapter03/Arrays/Program.cs
+++ vscode/Chapter03/Arrays/Program.cs
@@ -106,3 +106,4 @@ static string CheckSwitch(int[] values) => values switch
   [2, .. int[] others] => $"Starts with 2, then {others.Length} more numbers.",
   [..] => "Any items in any order.",
 };
+ 
